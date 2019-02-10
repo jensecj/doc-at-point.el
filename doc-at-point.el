42c1d9e61fff2ebb2383876a4200199568f7cc9f
Jens Christian Jensen <jensecj@gmail.com>
2019-02-10 16:46:39 +0100

    added config for showing doc in a `posframe.el` frame

--- doc-at-point.el
+++ doc-at-point.el
@@ -71,6 +71,58 @@ checked for if they should run from lowest order to highest. ")
 
   (setq doc-at-point-display-fn #'doc-at-point--display-with-popup))
 
+(with-eval-after-load 'posframe
+  (defvar doc-at-point--posframe "*doc-at-point-posframe*"
+    "Posframe used for displaying documentation.")
+
+  (defvar doc-at-point--posframe-font (frame-parameter (selected-frame) 'font)
+    "Font used in child frame, default is the same font as the
+      main-frame.")
+
+  (defvar doc-at-point--posframe-format "\n%s\n﻿"
+    "Format string for doc-strings to be displayed in the
+    posframe.  By default there's a zero-width non-breaking space
+    at the end of the format-string, to create a newline before
+    and after the doc-string, and internally the height of the
+    frame is set using `fit-frame-to-buffer', which ignores
+    newlines and whitespace at the end of buffers. ")
+
+  (defvar doc-at-point--internal-border-width 1
+    "Width of the border around `doc-at-point--posframe', in pixels.")
+
+  (defvar doc-at-point--fringe-widths '(6 . 6)
+    "Width of the fringes of `doc-at-point--posframe', as a pair
+    of `(left-fringe-width . right-fringe-width)'")
+
+  (defvar doc-at-point--posframe-hide-hooks '(pre-command-hook)
+    "Hooks where the documentation frame should hide.")
+
+  (defun doc-at-point--posframe-one-shot-kill ()
+    "Kills `doc-at-point--posframe' once a hook from
+`doc-at-point--posframe-hide-hooks' triggers, and removes then removes
+trigger from those hooks."
+    (unless (eq this-command 'doc-at-point)
+      (posframe-delete doc-at-point--posframe)
+      (dolist (hook doc-at-point--posframe-hide-hooks)
+        (remove-hook hook #'doc-at-point--posframe-one-shot-kill))))
+
+  (defun doc-at-point--display-with-posframe (doc-string)
+    "Shows DOC-STRING in a `posframe.el' frame."
+    (when (not (s-blank-str? doc-string))
+      (posframe-show doc-at-point--posframe
+                     :string (format doc-at-point--posframe-format doc-string)
+                     :internal-border-width doc-at-point--internal-border-width
+                     :font doc-at-point--posframe-font
+                     :left-fringe (car doc-at-point--fringe-widths)
+                     :right-fringe (cdr doc-at-point--fringe-widths))
+
+      ;; kill the frame once we trigger a hook from
+      ;; `doc-at-point--posframe-hide-hooks'.
+      (dolist (hook doc-at-point--posframe-hide-hooks)
+        (add-hook hook #'doc-at-point--posframe-one-shot-kill nil t))))
+
+  ;; use posframe as the default display-fn.
+  (setq doc-at-point-display-fn #'doc-at-point--display-with-posframe))
 
 (defun doc-at-point--should-run-p (should-run-sym-or-fn)
   "Return whether or not SHOULD-RUN-SYM-OR-FN indicates if
